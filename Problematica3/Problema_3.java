47dd3e086f3859df98b96adf26323550b90dd655
Daniel Aldana <230110150@itsoeh.edu.mx>
2023-11-19 21:39:45 -0600

    Resolucion inicial

--- Problematica3/Problema_3.java
+++ Problematica3/Problema_3.java
@@ -1,5 +1,41 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lldan
+ */
 public class Problema_3 {
     public static void main(String[] args) {
-        System.out.println("Hola, este es mi primer commit en GitHub.");
+        try (Scanner scanner = new Scanner(System.in)) {
+            // Solicitar al usuario las coordenadas del centro de la circunferencia
+            System.out.print("Ingrese la coordenada x del centro de la circunferencia: ");
+            double x1 = scanner.nextDouble();
+
+            System.out.print("Ingrese la coordenada y del centro de la circunferencia: ");
+            double y1 = scanner.nextDouble();
+
+            // Solicitar al usuario el radio de la circunferencia
+            System.out.print("Ingrese el radio de la circunferencia: ");
+            double r = scanner.nextDouble();
+
+            // Solicitar al usuario las coordenadas del punto a verificar
+            System.out.print("Ingrese la coordenada x del punto a verificar: ");
+            double x2 = scanner.nextDouble();
+
+            System.out.print("Ingrese la coordenada y del punto a verificar: ");
+            double y2 = scanner.nextDouble();
+
+            // Calcular la distancia entre el centro de la circunferencia y el punto dado
+            double distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+            // Verificar si el punto está dentro de la circunferencia
+            if (distancia <= r) {
+                System.out.println("El punto está dentro de la circunferencia.");
+            } else {
+                System.out.println("El punto está fuera de la circunferencia.");
+            }
+        }
+
     }
 }
