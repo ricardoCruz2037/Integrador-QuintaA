656fd2997b5c6f3f4b0b6e335bd6bfede2668a42
Valeria Soto Hernandez <230110118@itsoeh.edu.mx>
2023-11-21 22:14:21 -0600

    Ejercicio 6
    Se agregó el ejercicio 6, puede que con el tiempo existan cambios

---
+++ Codigos/Ejercicio 6/Ejercicio_6.java
@@ -0,0 +1,255 @@
+import java.util.Scanner;
+public class Ejercicio06Integrador {
+    public static void main(String[] args) {
+       Scanner d=new Scanner(System.in);
+        System.out.println("Ingresa la cantidad de bits");
+        int numB=d.nextInt();
+        if(numB<=5){
+        int combinaciones=(int) Math.pow(2, numB);
+        //Imprimir encabezado de la tabla
+        for(int i=0; i< numB ;i++){
+        System.out.print("Bit "+(numB - i - 1)+"\t");
+        }
+        System.out.println("Salidas");
+        
+        //Imprimir separador
+        for(int i=0; i< numB + 1;i++){
+        System.out.print("-------");
+        }
+        System.out.println();
+        //Combinacion de bits posibles
+        for(int i=0;i< combinaciones ;i++){
+        for(int j=0;j< numB;j++){
+        int bit=(i>>j)&1;
+        System.out.print(bit+"\t");   
+        }
+        System.out.println(i);
+        }
+        System.out.println("Teclea la cantidad de salidas que deseas sean verdaderas(1)");
+        int salidasC=d.nextInt();
+        int [] salidas=new int [salidasC];
+        for(int i=0;i< salidasC;i++){
+        System.out.print("Teclea la salida No. "+i+" : ");
+        salidas[i]=d.nextInt();
+        }
+        
+        System.out.println("La expresion es: ");
+        switch(numB){
+            case 2:{
+           for(int i=0;i<salidasC;i++){
+               if(salidas[i]==0){
+                   System.out.println("¬A ¬B");
+               }
+               if(salidas[i]==1){
+                   System.out.println("A ¬B");
+               }
+               if(salidas[i]==2){
+                   System.out.println("¬A B");
+               }
+               if(salidas[i]==3){
+                   System.out.println("A B");
+               }
+               if(i+1<salidasC){
+                   System.out.println(" + ");
+               }
+            }}
+            break;
+            
+            case 3:{
+           for(int i=0;i<salidasC;i++){
+               if(salidas[i]==0){
+                   System.out.println("¬A ¬B ¬C");
+               }
+               if(salidas[i]==1){
+                   System.out.println("A ¬B ¬C");
+               }
+               if(salidas[i]==2){
+                   System.out.println("¬A B ¬C");
+               }
+               if(salidas[i]==3){
+                   System.out.println("A B ¬C");
+               }
+               if(salidas[i]==4){
+                   System.out.println("¬A ¬B C");
+               }
+               if(salidas[i]==5){
+                   System.out.println("A ¬B C");
+               }
+               if(salidas[i]==6){
+                   System.out.println("¬A B C");
+               }
+               if(salidas[i]==7){
+                   System.out.println("A B C");
+               }
+               if(i+1<salidasC){
+                   System.out.println(" + ");
+               }
+            }}
+            break;
+            
+            case 4:{
+           for(int i=0;i<salidasC;i++){
+               if(salidas[i]==0){
+                   System.out.println("¬A ¬B ¬C ¬D");
+               }
+               if(salidas[i]==1){
+                   System.out.println("A ¬B ¬C ¬D");
+               }
+               if(salidas[i]==2){
+                   System.out.println("¬A B ¬C ¬D");
+               }
+               if(salidas[i]==3){
+                   System.out.println("A B ¬C ¬D");
+               }
+               if(salidas[i]==4){
+                   System.out.println("¬A ¬B C ¬D");
+               }
+               if(salidas[i]==5){
+                   System.out.println("A ¬B C ¬D");
+               }
+               if(salidas[i]==6){
+                   System.out.println("¬A B C ¬D");
+               }
+               if(salidas[i]==7){
+                   System.out.println("A B C ¬D");
+               }
+               if(salidas[i]==8){
+                   System.out.println("¬A ¬B ¬C D");
+               }
+               if(salidas[i]==9){
+                   System.out.println("A ¬B ¬C D");
+               }
+               if(salidas[i]==10){
+                   System.out.println("¬A B ¬C D");
+               }
+               if(salidas[i]==11){
+                   System.out.println("A B ¬C D");
+               }
+               if(salidas[i]==12){
+                   System.out.println("¬A ¬B C D");
+               }
+               if(salidas[i]==13){
+                   System.out.println("A ¬B C D");
+               }
+               if(salidas[i]==14){
+                   System.out.println("¬A B C D");
+               }
+               if(salidas[i]==15){
+                   System.out.println("A B C D");
+               }
+               if(i+1<salidasC){
+                   System.out.println(" + ");
+               }
+            }}
+            break;
+            
+            case 5:{
+           for(int i=0;i<salidasC;i++){
+               
+               if(salidas[i]==0){
+                   System.out.println("¬A ¬B ¬C ¬D ¬E");
+               }
+               if(salidas[i]==1){
+                   System.out.println("A ¬B ¬C ¬D ¬E");
+               }
+               if(salidas[i]==2){
+                   System.out.println("¬A B ¬C ¬D ¬E");
+               }
+               if(salidas[i]==3){
+                   System.out.println("A B ¬C ¬D ¬E");
+               }
+               if(salidas[i]==4){
+                   System.out.println("¬A ¬B C ¬D ¬E");
+               }
+               if(salidas[i]==5){
+                   System.out.println("A ¬B C ¬D ¬E");
+               }
+               if(salidas[i]==6){
+                   System.out.println("¬A B C ¬D ¬E");
+               }
+               if(salidas[i]==7){
+                   System.out.println("A B C ¬D ¬E");
+               }
+               if(salidas[i]==8){
+                   System.out.println("¬A ¬B ¬C D ¬E");
+               }
+               if(salidas[i]==9){
+                   System.out.println("A ¬B ¬C D ¬E");
+               }
+               if(salidas[i]==10){
+                   System.out.println("¬A B ¬C D ¬E");
+               }
+               if(salidas[i]==11){
+                   System.out.println("A B ¬C D ¬E");
+               }
+               if(salidas[i]==12){
+                   System.out.println("¬A ¬B C D ¬E");
+               }
+               if(salidas[i]==13){
+                   System.out.println("A ¬B C D ¬E");
+               }
+               if(salidas[i]==14){
+                   System.out.println("¬A B C D ¬E");
+               }
+               if(salidas[i]==15){
+                   System.out.println("A B C D ¬E");
+               }
+               if(salidas[i]==16){
+                   System.out.println("¬A ¬B ¬C ¬D E");
+               }
+               if(salidas[i]==17){
+                   System.out.println("A ¬B ¬C ¬D E");
+               }
+               if(salidas[i]==18){
+                   System.out.println("¬A B ¬C ¬D E");
+               }
+               if(salidas[i]==19){
+                   System.out.println("A B ¬C ¬D E");
+               }
+               if(salidas[i]==20){
+                   System.out.println("¬A ¬B C ¬D E");
+               }
+               if(salidas[i]==21){
+                   System.out.println("A ¬B C ¬D E");
+               }
+               if(salidas[i]==22){
+                   System.out.println("¬A B C ¬D E");
+               }
+               if(salidas[i]==23){
+                   System.out.println("A B C ¬D E");
+               }
+               if(salidas[i]==24){
+                   System.out.println("¬A ¬B ¬C D E");
+               }
+               if(salidas[i]==25){
+                   System.out.println("A ¬B ¬C D E");
+               }
+               if(salidas[i]==26){
+                   System.out.println("¬A B ¬C D E");
+               }
+               if(salidas[i]==27){
+                   System.out.println("A B ¬C D E");
+               }
+               if(salidas[i]==28){
+                   System.out.println("¬A ¬B C D E");
+               }
+               if(salidas[i]==29){
+                   System.out.println("A ¬B C D E");
+               }
+               if(salidas[i]==30){
+                   System.out.println("¬A B C D E");
+               }
+               if(salidas[i]==31){
+                   System.out.println("A B C D E");
+               }
+               if(i+1<salidasC){
+                   System.out.println(" + ");
+               }
+            }}
+        }
+        }else{
+        System.out.println("Lo lamentamos mucho, este programa solo es capas de leer un maximo de 5 bits, lamentamos los inconvenientes, lo solucionaremos pronto");
+        }
+    }
+    
+}
