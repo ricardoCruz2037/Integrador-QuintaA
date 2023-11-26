f2d5a984ed7882c34c5385f70660c4a28b30cd38
Dani-Ald <148412221+Dani-Ald@users.noreply.github.com>
2023-11-25 21:32:10 -0600

    Cambio minimo
    Se cambiaron algunos comentarios en el codigo

--- Codigo/Ejercicio-3/Ejercicio_3.java
+++ Codigo/Ejercicio-3/Ejercicio_3.java
@@ -2,17 +2,19 @@
 import java.util.Scanner;
 
 /**
- *
- * @author lldan
+ * PROBLEMA 3
+ * Dada una circunferencia con centro en el punto C con coordenadas (x1, y1) y radio r, evaluar si un punto T 
+ * con coordenadas (x2, y2) está dentro del área de la circunferencia.
+ * @author Daniel Aldana Lopez
  */
 public class Ejercicio_3 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             // Solicitar al usuario las coordenadas del centro de la circunferencia
-            System.out.print("Ingrese la coordenada x del centro de la circunferencia: ");
+            System.out.print("Ingrese la coordenada x1 del centro de la circunferencia: ");
             double x1 = scanner.nextDouble();
 
-            System.out.print("Ingrese la coordenada y del centro de la circunferencia: ");
+            System.out.print("Ingrese la coordenada y1 del centro de la circunferencia: ");
             double y1 = scanner.nextDouble();
 
             // Solicitar al usuario el radio de la circunferencia
@@ -20,20 +22,21 @@ public class Ejercicio_3 {
             double r = scanner.nextDouble();
 
             // Solicitar al usuario las coordenadas del punto a verificar
-            System.out.print("Ingrese la coordenada x del punto a verificar: ");
+            System.out.print("Ingrese la coordenada x2 del punto a verificar: ");
             double x2 = scanner.nextDouble();
 
-            System.out.print("Ingrese la coordenada y del punto a verificar: ");
+            System.out.print("Ingrese la coordenada y2 del punto a verificar: ");
             double y2 = scanner.nextDouble();
 
             // Calcular la distancia entre el centro de la circunferencia y el punto dado
             double distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 
-            // Verificar si el punto está dentro de la circunferencia
+            // Verificar si el punto está dentro de la circunferencia. Si la distancia es menor o igual a 
+            // El radio el punto T esta dentro de la circunferencia. De lo contrario el punto T esta fuera.
             if (distancia <= r) {
-                System.out.println("El punto está dentro de la circunferencia.");
+                System.out.println("El punto T está dentro de la circunferencia.");
             } else {
-                System.out.println("El punto está fuera de la circunferencia.");
+                System.out.println("El punto T está fuera de la circunferencia.");
             }
         }
 
